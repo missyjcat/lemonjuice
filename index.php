828bb2c735395d448029a53e523d158e97617c56
missyjcat <missyjcat@gmail.com>
2013-08-21 00:20:11 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lemonjuice</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+	<div class="wrapper">
+
+		<div class="top-fixed">
+		
+			<div class="social-media">
+				<img src="img/facebook.png" />
+				<img src="img/i.png" />
+				<img src="img/twitter.png" />
+				<img src="img/v.png" />
+			</div><!-- /social-media -->
+
+			<div class="logo float-left">
+				<a href="/"><img src="img/lemonjuice-logo.jpg" /></a>
+			</div><!-- /logo -->
+
+			<div class="nav float-left">
+				<ul>
+					<li><a href="work.php">Work</a></li>
+					<li><a href="news.php">News</a></li>
+					<li><a href="interactive">Interactive</a></li>
+					<li><a href="profile">Profile</a></li>
+					<li><a href="contact">Contact</a></li>
+				</ul>
+			</div><!-- /nav -->
+
+		</div><!-- /top-fixed -->
+
+		<div class="section work-section">
+			<div class="size-50">
+				<img src="top-work1.jpg" />
+			</div>
+			<div class="size-50">
+				<img src="top-work1.jpg" />
+			</div>
+		</div><!-- /section -->
+
+		<div class="section-title">
+			<h2>Freshly Squeezed</h2>
+		</div>
+
+		<div class="section freshly-squeezed-section">
+			<div class="size-33 first">
+				<img src="fs-work1.jpg" />
+			</div>
+			<div class="size-33 second">
+				<img src="fs-work2.jpg" />
+			</div>
+			<div class="size-33 third">
+				<img src="fs-work3.jpg" />
+			</div>
+		</div>
+
+		<div class="section-title">
+			<h2>Interactive</h2>
+		</div>
+
+		<div class="section freshly-squeezed-section">
+
+			<div class="row">
+				<div class="size-25 first">
+					<img src="interactive-work1.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work2.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work3.jpg" />
+				</div>
+				<div class="size-25 last">
+					<img src="interactive-work4.jpg" />
+				</div>
+			</div><!-- /row -->
+
+			<div class="row">
+				<div class="size-25 first">
+					<img src="interactive-work1.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work2.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work3.jpg" />
+				</div>
+				<div class="size-25 last">
+					<img src="interactive-work4.jpg" />
+				</div>
+			</div><!-- /row -->
+
+			<div class="row">
+				<div class="size-25 first">
+					<img src="interactive-work1.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work2.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work3.jpg" />
+				</div>
+				<div class="size-25 last">
+					<img src="interactive-work4.jpg" />
+				</div>
+			</div><!-- /row -->
+
+			<div class="row">
+				<div class="size-25 first">
+					<img src="interactive-work1.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work2.jpg" />
+				</div>
+				<div class="size-25">
+					<img src="interactive-work3.jpg" />
+				</div>
+				<div class="size-25 last">
+					<img src="interactive-work4.jpg" />
+				</div>
+			</div><!-- /row -->
+
+		</div><!-- /section -->
+
+		<div class="section-title">
+			<h2>More News</h2>
+		</div>
+
+	</div><!-- /wrapper -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/script.js">
+</body>
+</html>
